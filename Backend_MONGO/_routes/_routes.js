994b7285e82db29a541c14e19f0399e3e54a8768
IvanYuantama <123520791+IvanYuantama@users.noreply.github.com>
2024-05-26 11:36:55 +0700

    Delete Backend_MONGO directory

--- Backend_MONGO/_routes/_routes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const userRouter = require("./userRoute.js");
-const gameRouter = require("./gameRoute.js");
-
-const router = express.Router();
-
-router.use("/user", userRouter);
-router.use("/game", gameRouter);
-module.exports = router;
